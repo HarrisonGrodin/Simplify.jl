e36662e9708b94f9c339cd05f98abf88bc08b11b
Harrison Grodin <harrison.grodin@gmail.com>
2018-06-28 19:14:57 -0400

    Add `normalize` function

--- src/SymReduce.jl
+++ src/SymReduce.jl
@@ -1,8 +1,25 @@
 module SymReduce
 
-export Patterns
+export Patterns, @term, normalize
 
 
 include("patterns/Patterns.jl")
+using .Patterns
+
+normalize(rs) = Base.Fix2(normalize, rs)
+function normalize(t::Term, (l, r)::Pair)
+    σ = match(l, t)
+    σ === nothing && return t
+    σ(r)
+end
+function normalize(t::Term, rs)
+    while true
+        t = map(normalize(rs), t)
+        t′ = foldl(normalize, t, rs)
+        t == t′ && return t
+        t = t′
+    end
+end
+normalize(t::Term) = normalize(t, rules())
 
 end # module

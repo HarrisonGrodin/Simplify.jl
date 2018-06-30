bf4f261dde6e309da5070e85dc5e9c364595717a
Harrison Grodin <harrison.grodin@gmail.com>
2018-06-30 11:27:06 -0400

    Add boolean algebra rules

--- src/rules.jl
+++ src/rules.jl
@@ -7,3 +7,23 @@ rules(::Val{:STANDARD}) = @term PAIRS [
     x + 0 => x,
     0 + x => x,
 ]
+
+
+rules(::Val{:BOOLEAN}; and=:&, or=:|, neg=:!) = @term PAIRS [
+    $or(x, false) => x,
+    $and(x, true) => x,
+
+    $or(x, true) => true,
+    $and(x, false) => false,
+
+    $or(x, x) => x,
+    $and(x, x) => x,
+
+    $or(x, $and(x, y)) => x,
+    $and(x, $or(x, y)) => x,
+
+    $or(x, $neg(x)) => true,
+    $and(x, $neg(x)) => false,
+
+    $neg($neg(x)) => x,
+]

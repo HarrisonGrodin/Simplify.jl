a4922cb4d60796745f6134bfca3bbde5bdf2ae5c
Harrison Grodin <harrison.grodin@gmail.com>
2018-07-15 23:56:20 -0400

    Allow parsing function calls to custom types
    Currently based on function name suffix.

--- src/patterns/Patterns.jl
+++ src/patterns/Patterns.jl
@@ -17,13 +17,22 @@ Base.string(t::Term) = string(parse(t))
 include("types.jl")
 
 
+const SUFFIXES = Pair{String,Type}[
+    "" => Fn,
+]
+function _type(f::Symbol)
+    name = string(f)
+    suffix, typ = SUFFIXES[findfirst(p -> endswith(name, p[1]), SUFFIXES)]
+    typ{Symbol(name[1:end-length(suffix)])}
+end
+
 Base.parse(::Type{Term}, t::Term) = t
 Base.parse(::Type{Term}, n) = Constant(n)
 Base.parse(::Type{Term}, x::Symbol) = Variable(string(x))
 function Base.parse(::Type{Term}, ex::Expr)
     ex.head == :$    && return :(parse(Term, $(esc(ex.args[1]))))
     ex.head == :call || return Expr(ex.head, parse.(Term, ex.args)...)
-    :(Fn{$(Meta.quot(ex.args[1]))}($(parse.(Term, ex.args[2:end])...)))
+    :(_type($(Meta.quot(ex.args[1])))($(parse.(Term, ex.args[2:end])...)))
 end
 
 macro term(ex)

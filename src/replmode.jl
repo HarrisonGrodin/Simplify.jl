8369967a58a9852fd342428d8061cb6bba3ee37a
Harrison Grodin <harrison.grodin@gmail.com>
2018-08-22 15:48:06 -0400

    Condense parsing logic
    Uses `convert(Term, ex)` rather than `@term` macro as a more direct approach.

--- src/replmode.jl
+++ src/replmode.jl
@@ -1,18 +1,15 @@
-module replmode
+module REPLMode
 
 using ReplMaker
 
-function rewrite_parser(s)
-    ex = Meta.parse(s)
-    quote
-        normalize(@term $ex)
-    end
+initrepl(
+    prompt_text  = "@term> ",
+    prompt_color = :blue, 
+    start_key    = '=', 
+    mode_name    = "Rewrite_mode",
+) do s
+    term = convert(Term, Meta.parse(s))
+    :(normalize($term))
 end
-
-initrepl(rewrite_parser, 
-         prompt_text="@term> ",
-         prompt_color = :blue, 
-         start_key='=', 
-         mode_name="Rewrite_mode")
 
 end

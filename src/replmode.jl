8daabffb90913a004a9b966837d074e40999bbda
MasonProtter <mason.protter@gmail.com>
2018-08-24 10:41:28 -0600

    allow user to specify normalization rules

--- src/replmode.jl
+++ src/replmode.jl
@@ -3,11 +3,12 @@ using ReplMaker
 if isdefined(Base, :active_repl)
     initrepl(
         prompt_text  = "@term> ",
-        prompt_color = :blue, 
+        prompt_color = :cyan, 
         start_key    = '=', 
         mode_name    = "Rewrite_mode",
     ) do s
-        ex = Meta.parse(s)
-        :(normalize(@term $(ex)))
+        exs = [Meta.parse(i) for i in split(s, "with")]
+        :(normalize((@term $(exs[1])), $(exs[2])))
     end
 end
+

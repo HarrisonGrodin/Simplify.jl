69d3d6235c216fafca30a492e36877d9535f206a
Harrison Grodin <harrison.grodin@gmail.com>
2019-01-03 11:41:19 -0500

    Revert #63

--- src/rule.jl
+++ src/rule.jl
@@ -1,4 +1,4 @@
-using DiffRules, Memoize
+using DiffRules
 
 export Rules
 export normalize
@@ -26,7 +26,7 @@ Base.vcat(rss::Rules...) = Rules([(rs.rules for rs ∈ rss)...;])
 
 
 normalize(rs::Rules) = Base.Fix2(normalize, rs)
-@memoize function normalize(t::Term, rs::Rules)
+function normalize(t::Term, rs::Rules)
     while true
         t = map(normalize(rs), t)  # FIXME: replace with `subexpressions`
         t′ = foldl(normalize, rs; init=t)
